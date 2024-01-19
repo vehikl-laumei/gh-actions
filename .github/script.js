37b165cd2aa41895183b6ba5d08add8616719d18
vehikl-laumei <119354322+vehikl-laumei@users.noreply.github.com>
2024-01-19 12:27:16 -0500

    Update script.js

--- .github/script.js
+++ .github/script.js
@@ -1,45 +1,47 @@
-// Function to format commit message
-function formatCommitMessage(commitMessage) {
-  const teamTicketRegex = /^\[([A-Z]+-\d+)\]/;
-  const prNumberRegex = /#(\d+)$/;
+module.exports = async ({ github, context }) => {
+  // Function to format commit message
+  function formatCommitMessage(commitMessage) {
+    const teamTicketRegex = /^\[([A-Z]+-\d+)\]/;
+    const prNumberRegex = /#(\d+)$/;
 
-  const teamTicketMatch = commitMessage.match(teamTicketRegex);
-  const prNumberMatch = commitMessage.match(prNumberRegex);
+    const teamTicketMatch = commitMessage.match(teamTicketRegex);
+    const prNumberMatch = commitMessage.match(prNumberRegex);
 
-  if (teamTicketMatch && prNumberMatch) {
-    const teamTicket = teamTicketMatch[1];
-    const prNumber = prNumberMatch[1];
-    return `[[${teamTicket}](https://vshred.atlassian.net/browse/${teamTicket})]: ${commitMessage} ([#${prNumber}](https://github.com/VinsanityShred/laravel-ecommerce/pull/${prNumber}))`;
+    if (teamTicketMatch && prNumberMatch) {
+      const teamTicket = teamTicketMatch[1];
+      const prNumber = prNumberMatch[1];
+      return `[[${teamTicket}](https://vshred.atlassian.net/browse/${teamTicket})]: ${commitMessage} ([#${prNumber}](https://github.com/VinsanityShred/laravel-ecommerce/pull/${prNumber}))`;
+    }
+
+    return null;
   }
 
-  return null;
-}
+  // Get list of commits in the PR
+  const pull_number = context.payload.pull_request.number;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
 
-// Get list of commits in the PR
-const pull_number = context.payload.pull_request.number;
-const owner = context.repo.owner;
-const repo = context.repo.repo;
-
-const listCommitsResponse = await github.rest.pulls.listCommits({
-  owner,
-  repo,
-  pull_number,
-});
-
-// Filter and format commits
-const formattedCommits = listCommitsResponse.data
-  .map(commit => formatCommitMessage(commit.commit.message))
-  .filter(Boolean); // Filter out null values
-
-// Construct new PR description only if there are formatted commits
-let newDescription = formattedCommits.length > 0 && formattedCommits.join('\n');
-
-// Update the PR description if necessary
-if (newDescription) {
-  await github.rest.pulls.update({
+  const listCommitsResponse = await github.rest.pulls.listCommits({
     owner,
     repo,
     pull_number,
-    body: newDescription,
   });
-}
+
+  // Filter and format commits
+  const formattedCommits = listCommitsResponse.data
+    .map(commit => formatCommitMessage(commit.commit.message))
+    .filter(Boolean); // Filter out null values
+
+  // Construct new PR description only if there are formatted commits
+  let newDescription = formattedCommits.length > 0 ? formattedCommits.join('\n') : '';
+
+  // Update the PR description if necessary
+  if (newDescription) {
+    await github.rest.pulls.update({
+      owner,
+      repo,
+      pull_number,
+      body: newDescription,
+    });
+  }
+};
